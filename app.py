1601d54cf430e2072e036dace7e4c5126525913d
ryan-leal <ryanxd178@gmail.com>
2024-07-23 21:41:41 -0300

    Added HTML/CSS

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
+from markupsafe import escape
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def index():
+    return render_template("home.html")
+
+@app.route("/test/<name>")
+def testFunc(name):
+    return f"Hello, {escape(name)}"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
